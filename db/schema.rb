c5b9df81c2b0f3df0b571df5bda4bec04d1f2957
Tyler Walker <tyler.p.walker@wsu.edu>
2015-10-01 16:24:56 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001222632) do
+ActiveRecord::Schema.define(version: 20151001225524) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20151001222632) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "studentid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

8985ba23b5aa4f169b8cfad6c28004d2c8ae495c
Tyler Walker <tyler.p.walker@wsu.edu>
2015-10-05 17:25:01 -0700

    Adding questions controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,24 @@
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end

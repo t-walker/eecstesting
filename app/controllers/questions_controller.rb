9d531f23328d933159ccc31c9f728c21d5a12036
Tyler Walker <twalker@eecs.wsu.edu>
2015-10-05 18:37:37 -0700

    Finishing up Questions controller/views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
   def index
     @questions = Question.all
   end
@@ -8,17 +9,42 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question.new(question_params)
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
-  end
+    respond_to do |format|
+      if @question.update(question_params)
+        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+        format.json { render :show, status: :ok, location: @question }
+      else
+        format.html { render :edit }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end  end
 
   def destroy
+    @question.destroy
+    respond_to do |format|
+      format.html { redirect_to questions_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+private
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:prompt, :possible1, :possible2, :possible3, :possible4, :correct)
   end
 end

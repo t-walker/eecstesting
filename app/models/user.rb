69070e4a8eb2044970e67130752d6354e9c8101b
Tyler Walker <tyler.p.walker@wsu.edu>
2015-10-07 18:15:06 -0700

    updating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  enum role: [:user, :admin]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :usertests
   has_many :responses, through: :usertests
+
 
 end

50d5f7462d46c192e72694b6e17ed552115f350e
Tyler Walker <tyler.p.walker@wsu.edu>
2015-10-07 18:14:16 -0700

    Adding admin/user role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007042633) do
+ActiveRecord::Schema.define(version: 20151008010841) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "prompt"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20151007042633) do
     t.string   "firstname"
     t.string   "lastname"
     t.string   "studentid"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

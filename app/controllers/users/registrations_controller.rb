185edceabfa4cc73b5b6a5f4b17af249278504ed
Tyler Walker <tyler.p.walker@wsu.edu>
2015-10-08 10:52:38 -0700

    Hotfix for user params issues

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  skip_load_and_authorize_resource
   before_filter :configure_permitted_parameters
+  skip_load_and_authorize_resource
 
   protected
 
--- app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < ApplicationController
-  load_and_authorize_resource
-
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
-  end
-end

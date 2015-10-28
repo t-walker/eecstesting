adb2b9fdee06990d55949b4657c03c4563f2cbc3
Tyler Walker <twalker@eecs.wsu.edu>
2015-10-27 18:01:29 -0700

    Can edit testversion date

--- app/controllers/testversions_controller.rb
+++ app/controllers/testversions_controller.rb
@@ -13,16 +13,19 @@ class TestversionsController < ApplicationController
 
   def edit
     @testversion = Testversion.find(params[:id])
+    @testversion.startdate = @testversion.startdate.strftime("%m/%d/%Y")
+    @testversion.enddate = @testversion.enddate.strftime("%m/%d/%Y")
+
     @questions = Question.all
   end
 
   def create
     @testversion = Testversion.new(testversion_params)
-    @testversion.startdate = Date.parse(@testversion.startdate.to_s)
-    @testversion.enddate = Date.parse(@testversion.enddate.to_s)
+    @testversion.startdate = Date.strptime(testversion_params[:startdate], "%m/%d/%Y")
+    @testversion.enddate = Date.strptime(testversion_params[:enddate], "%m/%d/%Y")
     respond_to do |format|
       if @testversion.save
-        format.html { redirect_to usertests_url, notice: 'testversion was successfully created.' }
+        format.html { redirect_to usertests_url, notice: 'Test Version was successfully created.' }
       else
         format.html { render :new }
       end
@@ -30,9 +33,14 @@ class TestversionsController < ApplicationController
   end
 
   def update
+    x = testversion_params
+    x[:startdate] = Date.strptime(x[:startdate], "%m/%d/%Y").strftime("%d/%m/%Y")
+    puts x[:startdate]
+    x[:enddate] = Date.strptime(x[:enddate], "%m/%d/%Y").strftime("%d/%m/%Y")
+    puts x[:enddate]
     respond_to do |format|
-      if @testversion.update(testversion_params)
-        format.html { redirect_to usertests_url, notice: 'testversion was successfully updated.' }
+      if @testversion.update(x)
+        format.html { redirect_to usertests_url, notice: 'Test Version was successfully updated.' }
       else
         format.html { render :edit }
       end

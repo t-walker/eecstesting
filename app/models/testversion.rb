53bdaf69cea12785d9e649a413ec2fc36631d066
Tyler Walker <twalker@eecs.wsu.edu>
2015-10-30 11:22:12 -0700

    Dates and uniqueness

--- app/models/testversion.rb
+++ app/models/testversion.rb
@@ -2,14 +2,26 @@ class Testversion < ActiveRecord::Base
   has_and_belongs_to_many :questions, dependent: :destroy
   accepts_nested_attributes_for :questions
   has_many :usertests, dependent: :destroy
-  validates_uniqueness_of :isopen, if: :isopen
-
   validates :title, presence: true
   validates :startdate, presence: true
   validates :enddate, presence: true
 
-  def is_open?
-    isopen
+  scope :active, ->{where(:isopen => true)}
+
+  def self.active_tests?
+    if Testversion.active.size > 1
+      return true
+    else
+      return false
+    end
+  end
+
+  def open?
+    if isopen && startdate <= Date.today && Date.today <= enddate
+      true
+    else
+      false
+    end
   end
 
   def self.current

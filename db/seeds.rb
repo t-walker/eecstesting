2bc94eb15b1e2291a3693826ab55cc1b0c70e71a
Tyler Walker <twalker@eecs.wsu.edu>
2015-10-31 14:36:46 -0700

    fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Response.delete_all
-Question.delete_all
 Usertest.delete_all
 Testversion.delete_all
 Notification.delete_all
 User.delete_all
-
-Question.create(prompt: "What is the current year",
-                possible1: "2012",
-                possible2: "2013",
-                possible3: "2015",
-                possible4: "2014",
-                correct: "2015",
-                question_type: "multchoice")
-
-Question.create(prompt: "What is the next year",
-                possible1: "2012",
-                possible2: "2013",
-                possible3: "2016",
-                possible4: "2014",
-                correct: "2016",
-                question_type: "multchoice")
-
-Question.create(prompt: "Is def a reserved word in Python",
-                possible1: "True",
-                possible2: "False",
-                correct: "True",
-                question_type: "truefalse")
 
 User.create!(email: 'tyler.p.walker@wsu.edu',
              password: 'password1234',

d6d3c094ffd7f3ce6153e6c24c891e4c928371ef
Tyler Walker <twalkerweb@gmail.com>
2017-01-12 11:27:59 -0800

    Switching to Simplex

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,8 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( superhero.css )
-Rails.application.config.assets.precompile += %w( superhero.js )
+Rails.application.config.assets.precompile += %w( simplex.css )
+Rails.application.config.assets.precompile += %w( simplex.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
